90a07db081a9ed9700d4196a0117f22eeaf5cc43
elitaldev <eligoli1007@yahoo.co.uk>
2023-03-15 15:25:22 +0000

    added new file

--- todo/tests_models.py
+++ todo/tests_models.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Item
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    def test_done_defaults_fals(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
